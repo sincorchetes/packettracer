3cb817a7c0fdc5c079249a6de49c899c1eb8f8a0
Álvaro Castillo <sincorchetes@gmail.com>
2019-05-18 18:25:18 +0200

    Updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-wget -c https://github.com/sincorchetes/packettracer/blob/master/libcrypto.so.1.0.0?raw=true &
-wget -c  https://github.com/sincorchetes/packettracer/blob/master/libssl.so.1.0.0?raw=true
-
 sudo cp libcrypto.so.1.0.0 /opt/pt/bin && sudo cp libssl.so.1.0.0 /opt/pt/bin
 echo "Great"
